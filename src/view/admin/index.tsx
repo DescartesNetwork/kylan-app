c33a4f455fac1418275d758f28583283fbec75ab
thiennv <thiennguyen@descartes.network>
2022-02-28 18:37:35 +0700

    new printer

--- src/view/admin/index.tsx
+++ src/view/admin/index.tsx
@@ -1,10 +1,14 @@
 import { Col, Row } from 'antd'
 import Certificate from './certificate'
+import NewPrinter from './newPrinter'
 import OverView from './overView'
 
 const Admin = () => {
   return (
     <Row gutter={[24, 24]}>
+      <Col span={24}>
+        <NewPrinter />
+      </Col>
       <Col span={24}>
         <OverView />
       </Col>
