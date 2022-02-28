0a82b5e6f324c5df6655afa1fbcf3db7ef8c4b20
thiennv <thiennguyen@descartes.network>
2022-02-28 18:57:42 +0700

    remove new printer

--- src/view/admin/index.tsx
+++ src/view/admin/index.tsx
@@ -1,14 +1,10 @@
 import { Col, Row } from 'antd'
 import Certificate from './certificate'
-import NewPrinter from './newPrinter'
 import OverView from './overView'
 
 const Admin = () => {
   return (
     <Row gutter={[24, 24]}>
-      <Col span={24}>
-        <NewPrinter />
-      </Col>
       <Col span={24}>
         <OverView />
       </Col>
