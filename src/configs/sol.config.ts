f53196bbc4aa3608c5e8b552f847b94ca5845204
thiennv <thiennguyen@descartes.network>
2022-03-04 18:00:12 +0700

    printer address mainet

--- src/configs/sol.config.ts
+++ src/configs/sol.config.ts
@@ -14,7 +14,6 @@ type Conf = {
   swapAddress: string
   taxmanAddress: string
   printerAddress: string
-  stableTokenAddress: string
 } & typeof SOLVARS
 
 const conf: Record<Net, Conf> = {
@@ -27,9 +26,7 @@ const conf: Record<Net, Conf> = {
     chainId: 103,
     swapAddress: '4erFSLP7oBFSVC1t35jdxmbfxEhYCKfoM6XdG2BLR3UF',
     taxmanAddress: '8UaZw2jDhJzv5V53569JbCd3bD4BnyCfBH3sjwgajGS9',
-    // printerAddress: '3eTgCb3SXPCQZEUg6heAah2oD2f4qwjhacdUz3T5dc2s',
     printerAddress: 'FpsYwMmDKkQ4sjUF2Cpp3imzUo8MZGWVG93eSPmHCR7',
-    stableTokenAddress: 'Vp15BGrUdTKPNDW4rkFm4ksoPHDTcg16Q7p3fwR4SPZ',
   },
 
   /**
@@ -42,7 +39,6 @@ const conf: Record<Net, Conf> = {
     swapAddress: '',
     taxmanAddress: '',
     printerAddress: '',
-    stableTokenAddress: '',
   },
 
   /**
@@ -54,8 +50,7 @@ const conf: Record<Net, Conf> = {
     chainId: 101,
     swapAddress: 'SSW7ooZ1EbEognq5GosbygA3uWW1Hq1NsFq6TsftCFV',
     taxmanAddress: '9doo2HZQEmh2NgfT3Yx12M89aoBheycYqH1eaR5gKb3e',
-    printerAddress: 'EJuHBZ8G78wzuoaAxo3TxdZGnmEc9NxV8gvsNeANmPru',
-    stableTokenAddress: 'AT5CEW4zLMNwMEQPi9oSXSMAiHvX1bYXVpFi5JgwoMhx',
+    printerAddress: 'B45FZEMnvPH1zHkzCKGcQA75PPd47xBbBzFnXpN5Vnn7',
   },
 }
 
